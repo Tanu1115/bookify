9ed0d88ff79a9a262f302a0333d5b902e11ee009
tanu <tanukhrwar15@gmail.com>
2025-07-01 09:35:39 +0530

    updated code

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,18 @@
- exports.getLogin = (req,res) =>{
-    res.render('auth/login');
+ const getLogin = async (req,res) =>{
+    res.render('/auth/login');
 };
-exports.postLogin = (req,res,next) =>{
+const  postLogin = async (req,res) =>{
     res.redirect("/")
 }
+ const getSignup = async (req,res) =>{
+    res.render('/auth/signup');
+ };
+ const postSignup = async (req,res) =>{
+    res.render("/")
+ }
+ module.exports = {
+   getLogin,
+   postLogin,
+   getSignup,
+ postSignup
+ }
